7420efbc95f50b8f456ccf0c188e66deeef85934
ikarosilva00@gmail.com <ikarosilva00@gmail.com@e73a84c7-bd09-c1a9-13a7-bc916e2add03>
2013-06-12 21:55:11 +0000

    created matlab wrappers for scoring the challenge

--- mcode/score2013.m
+++ mcode/score2013.m
@@ -11,14 +11,14 @@ function varargout=score2013(varargin)
 %
 %Input Parameters:
 % recName    
-%       String specifying the WFDB record file.
+%       String specifying the binary WFDB record file.
 %
 % refAnn    
-%       String specifying the reference WFDB FQRS annotation file (should 
+%       String specifying the reference binary WFDB FQRS annotation file (should 
 %       be 'fqrs').
 %
 % testAnn    
-%       String specifying the test WFDB FQRS annotation file.
+%       String specifying the test binary WFDB FQRS annotation file.
 % 
 % 
 % Outputs:
@@ -29,71 +29,44 @@ function varargout=score2013(varargin)
 % score2
 %       1x1 Double preliminary results for Event 2/5.
 %
+% 
+% NOTE: 
+%    
+%      1) The function requires that the all files be binary (ie, the record
+%         file has a *.dat extension and *.hea header file, and the annotations files are WFDB
+%         annotations, not text files).
+%
+%      2) All the files must be located in the directory at which this
+%         function is being evaluated. You must also have write permission
+%         to this directory because the scoring process generates temporary
+%         files for the RR interval scoring step. These temporary filess
+%         are denoted by recName.rr_refAnn and recName.rr_testAnn. If 
+%         you answer is not in binary annotation format, you can use WRANN
+%         to convert it to this format. 
+%      
+%      3) The scoring can go quicker once the RR files for the reference 
+%         annotation (recName.rr_refAnn) from step (2) above has been
+%         generated for the first time. If the reference annotation
+%         changes for some reason, you should delete these temporary files
+%         and let the function re-generate them.
+%
+%
+% %Example :
+% %Gets score for recrod 'a01.dat', with reference annotation 'a01.fqrs'
+% %and user submitted answer 'a01.wfdbsample_entry1'
+% [score1,score2]=score2013('a01','fqrs','wfdbsample_entry1')
+%
 % Written by Ikaro Silva, 2013
 % Last Modified: -
 % Version 1.0
 %
 % Last modified June 11, 2013
 %
-% See also WRANN, TACH, MXM
+% See also WRANN, TACH, MXM, ANN2RR, 
 
-
-%Set default pararamter values
-inputs={'recName','refAnn','testAnn'};
-outputs={'score1','score2'};
-for n=1:nargin
-    if(~isempty(varargin{n}))
-        eval([inputs{n} '=varargin{n};'])
-    end
-end
-
-
-[hrFQRS]=tach(recName,refAnn);
-[hrTEST]=tach(recName,testAnn);
-score1=sqrt(mean((hrTEST-hrFQRS).^2));
-
-[RRFQRS,tmsFQRS]=ann2rr(recName,refAnn);
-[RRTEST,tmsTEST]=ann2rr(recName,refAnn);
-
-N=length(RRFQRS);
-strFQRS=cell(N+1);
-strFQRS(1)={'[LWEditLog-1.0] Record ' refAnn ', annotator rr_fqrs (1000 samples/second)'};
-for n=1:N-1
-   strFQRS(n+1)={[num2str(RRFQRS(n)) ',=,' num2str(tmsFQRS(n))]};
-end
-patchann(strFQRS);
-
-N=length(RRTEST);
-strTEST=cell(N+1);
-strTEST(1)={'[LWEditLog-1.0] Record ' testAnn ', annotator rr_fqrs (1000 samples/second)'};
-for n=1:N-1
-   strTEST(n+1)={[num2str(RRTEST(n)) ',=,' num2str(tmsTEST(n))]};
-end
-patchann(strTEST);
-
-mxm(recName,refAnn,testAnn);
-
-for n=1:nargout
-        eval(['varargout{n}=' outputs{n} ';'])
-end
-
-%(ann2rr -r $REC -a fqrs -c -V | field -ox 1 2 | sed "s/x/,=,/" ) | patchann
-% ( ann2rr -r $REC -a $TANN -c -V | field -ox 1 2 | sed "s/x/,=,/" ) | patchann
-% mxm -r $REC -a rr_fqrs rr_${TANN} -f 0 -l score2.$$ 2>/dev/null
-% 
-% if [[ ! -a score2.$$ ]];
-% then
-%    echo "Could no score events 2/5 while running the command: "
-%    echo "ann2rr -r ${REC} -a ${TANN} -c -V | field -ox 1 2 | sed "s/x/,=,/" ) | patchann
-% mxm -r $REC -a rr_fqrs rr_${TANN} -f 0"
-% fi
-% 
-% S1=`field -w 6 <score1.$$`
-% S2=`tail -1 <score2.$$ | field -w 2`
-
-function patchann(varargin)
 
 persistent javaWfdbExec
+
 if(~wfdbloadlib)
     %Add classes to dynamic path
     wfdbloadlib;
@@ -101,15 +74,19 @@ end
 
 if(isempty(javaWfdbExec))
     %Load the Java class in memory if it has not been loaded yet
-    javaWfdbExec=org.physionet.wfdb.Wfdbexec('patchann');
+    javaWfdbExec=org.physionet.wfdb.Score2013;
 end
 
-inputs={'strTmp'};
+%Set default pararamter values
+inputs={'recName','refAnn','testAnn'};
+outputs={'score(1)','score(2)'};
 for n=1:nargin
     if(~isempty(varargin{n}))
         eval([inputs{n} '=varargin{n};'])
     end
 end
-wfdb_argument={'strTmp'};
-javaWfdbExec.execWithStandardInput(wfdb_argument);
+score=javaWfdbExec.getScore({recName,refAnn,testAnn});
+for n=1:nargout
+        eval(['varargout{n}=' outputs{n} ';'])
+end
 
