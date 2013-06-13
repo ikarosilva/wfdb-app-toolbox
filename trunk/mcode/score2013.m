87db7a523543d3d48d5656896904b2ea5e80d807
ikarosilva00@gmail.com <ikarosilva00@gmail.com@e73a84c7-bd09-c1a9-13a7-bc916e2add03>
2013-06-13 13:38:17 +0000

    allowed scalar parameters into wrann.

--- trunk/mcode/score2013.m
+++ trunk/mcode/score2013.m
@@ -4,23 +4,23 @@ function varargout=score2013(varargin)
 %
 %
 % Scores the an entry to the PhysioNet 2013 Challenge (Noninvasive Fetal
-% ECG). 
+% ECG).
 %
 % NOTE: This function requires permission to write to the current directory
-% in order to store temporary files. 
+% in order to store temporary files.
 %
 %Input Parameters:
-% recName    
+% recName
 %       String specifying the binary WFDB record file.
 %
-% refAnn    
-%       String specifying the reference binary WFDB FQRS annotation file (should 
+% refAnn
+%       String specifying the reference binary WFDB FQRS annotation file (should
 %       be 'fqrs').
 %
-% testAnn    
+% testAnn
 %       String specifying the test binary WFDB FQRS annotation file.
-% 
-% 
+%
+%
 % Outputs:
 %
 % score1
@@ -29,26 +29,40 @@ function varargout=score2013(varargin)
 % score2
 %       1x1 Double preliminary results for Event 2/5.
 %
-% 
-% NOTE: 
-%    
-%      1) The function requires that the all files be binary (ie, the record
+%
+% NOTE:
+%
+%      1) This function requires that the all files be binary (ie, the record
 %         file has a *.dat extension and *.hea header file, and the annotations files are WFDB
-%         annotations, not text files).
+%         annotations, not text files). To convert your text annnotation to
+%         WDFB annotation run these steps (assuming you have an annotation file called a01.txt
+%         for record a01.dat :
+%                i. Make sure your signal records are reference
+%                annotations are in WFDB format (you should have three
+%                files per record, for record a01 they are : a01.dat, a01.hea, and a01.fqrs). 
+%                Make sure these files are on the same directory as your
+%                annotation. You can download the WFDB files from 
+%                        http://www.physionet.org/challenge/2013/#data-sets
+%               ii. Load your text annotation into MATLAB :
+%                       ann=dlmread('a01.txt');
+%              iii. Save it to a WFDB annotation, say "entry1" :
+%                       wrann('a01','entry1',x)
+%               iv. Score your results based on the file from step ii :
+%                       score2013('a01','fqrs','entry1'
 %
 %      2) All the files must be located in the directory at which this
 %         function is being evaluated. You must also have write permission
 %         to this directory because the scoring process generates temporary
-%         files for the RR interval scoring step. These temporary filess
-%         are denoted by recName.rr_refAnn and recName.rr_testAnn. If 
+%         files for the RR interval scoring step. These temporary files
+%         are denoted by recName.rr_refAnn and recName.rr_testAnn. If
 %         you answer is not in binary annotation format, you can use WRANN
-%         to convert it to this format. 
-%      
-%      3) The scoring can go quicker once the RR files for the reference 
-%         annotation (recName.rr_refAnn) from step (2) above has been
-%         generated for the first time. If the reference annotation
-%         changes for some reason, you should delete these temporary files
-%         and let the function re-generate them.
+%         to convert it to this format.
+%
+%      3) The scoring will go quicker once the RR files for the reference
+%         annotation (recName.rr_refAnn) from step (2) above have been
+%         generated for the first time. If the original reference annotations
+%         (recName.refAnn) changes for some reason, you should delete these
+%         temporary files and let the function re-generate them.
 %
 %
 % %Example :
@@ -62,7 +76,7 @@ function varargout=score2013(varargin)
 %
 % Last modified June 11, 2013
 %
-% See also WRANN, TACH, MXM, ANN2RR, 
+% See also WRANN, TACH, MXM, ANN2RR,
 
 
 persistent javaWfdbExec
@@ -85,8 +99,23 @@ for n=1:nargin
         eval([inputs{n} '=varargin{n};'])
     end
 end
-score=javaWfdbExec.getScore({recName,refAnn,testAnn});
+try
+    score=javaWfdbExec.getScore({recName,refAnn,testAnn});
+catch
+    if(~isempty(strfind(lasterr,'java.lang.AssertionError')))
+        warning(['Could not process your input file (see NOTE item #1 on help) !! Please make sure your ' recName '.' refAnn ...
+            ' and ' recName '.' testAnn ' are a binary WFDB file.' ...
+            ' Your can convert from text to WFDB binary by using WRANN.'])
+    elseif(~exist([recName '.dat']) || ~exist([recName '.hea']) || ~exist([recName '.fqrs']))
+        fprintf(['Warning: Could not score entry because you are missing required files.\n\t' ...
+            ' Make sure you have these file in your current directory: \n\t '...
+        recName '.dat\n\t ' recName '.hea\n\t ' recName '.fqrs\n' ...
+        'You can download the challenge files from:\n\t'  ...
+        'http://www.physionet.org/challenge/2013/#data-sets\n']);
+    end
+    error(lasterr)
+end
 for n=1:nargout
-        eval(['varargout{n}=' outputs{n} ';'])
+    eval(['varargout{n}=' outputs{n} ';'])
 end
 
