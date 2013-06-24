73a88921be3994d76bec18a576ebb2dec2e08367
ikarosilva00@gmail.com <ikarosilva00@gmail.com@e73a84c7-bd09-c1a9-13a7-bc916e2add03>
2013-06-24 15:10:41 +0000

    fixed unit test issue with LOMB.

--- trunk/UnitTests/test_lomb.m
+++ trunk/UnitTests/test_lomb.m
@@ -9,7 +9,7 @@ for n=1:nargin
 end
 
 %Test the examples 
-test_string={['[ann]=rdann(''mitdb/100'',''atr'');[Pxx,F]=lomb([tm(ann) signal(ann)]);'...
+test_string={['[tm, signal]=rdsamp(''mitdb/100'',1);[ann]=rdann(''mitdb/100'',''atr'');[Pxx,F]=lomb([tm(ann) signal(ann)]);'...
     'plot(F,Pxx);grid on;hold on']};
 clean_up={};
 [tests,pass,perf]=test_wrapper(test_string,clean_up,verbose);
