62670d42deca736dafedfa9f89015f19031f510f
ikarosilva00@gmail.com <ikarosilva00@gmail.com@e73a84c7-bd09-c1a9-13a7-bc916e2add03>
2013-06-26 21:31:17 +0000

    adding computing method for dfa

--- trunk/src/org/physionet/wfdb/concurrent/Dfa.java
+++ trunk/src/org/physionet/wfdb/concurrent/Dfa.java
@@ -4,23 +4,19 @@ import java.util.ArrayList;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Dfa implements Callable<Double>{
 	private static final int THREADS=Runtime.getRuntime().availableProcessors();
-	private final int  quotient;
-	private final int  remainder;
 	private final double[] data; //Once initialized should be read only
 	private final int[] scales; //Once initialized should be read only
-	private final int[] taskSize;
 	private final BlockingQueue<Integer> tasks;
 	private double[] results;
 
 	Dfa(double[] x, int[] scales) throws InterruptedException{
-		
+
 		//Zero mean the data, itegrate, and initialize it
 		double mx=0, integrator=0;
 		data = new double[x.length];
@@ -31,37 +27,25 @@ public class Dfa implements Callable<Double>{
 			integrator+=x[i]-mx;
 			data[i]=integrator;
 		}
-		
-		
+
 		//Set task queue according to scales to be calculated
 		this.scales=scales;
 		results=new double[scales.length];
-		
-		quotient=scales.length/THREADS;
-		remainder=scales.length%THREADS;
-		taskSize=new int[THREADS];
-		tasks=new ArrayBlockingQueue<Integer>(THREADS);
-		for(int i=0;i<THREADS;i++){
-			taskSize[i]=quotient;
-			if(remainder>i)
-				taskSize[i]++;
-			tasks.put(i*quotient);
-		} 
+		tasks=new ArrayBlockingQueue<Integer>(scales.length);
+		for(int i=0;i<scales.length;i++)
+			tasks.put(i);
+
 	}
 
 	public Double call(){
 		double x=0;
-		int start=0, end =0;
+		int scaleInd=0;
 		long id=Thread.currentThread().getId();
 		try {
-			start = tasks.take();
-			end=start+taskSize[start/quotient];
-			System.out.println(id + ": runing task from :" + start + 
-					" to " + (end-1));
-			
-			x=compute(start,end);
-			System.out.println(id + ": finished task from :" + start + 
-					" to " + end + " sum= " + x);
+			scaleInd = tasks.take();
+			System.out.println(id + ": runing scale:" + scales[scaleInd]);
+			results[scaleInd]=compute(scales[scaleInd]);
+			System.out.println(id + ": finished task from :" + scales[scaleInd]);
 		} catch (InterruptedException e) {
 			System.err.println("Could not access task queue");
 			e.printStackTrace();
@@ -69,19 +53,19 @@ public class Dfa implements Callable<Double>{
 		return x;
 	}
 
-	
-	
-	public double compute(int start, int end){
-		//Compute DFA within an array chunk
+
+
+	public double compute(int scale){
+		//Compute 
 		double x=0;
-		for(int i=start;i<end;++i){
-			x+=Math.sqrt(data[i]);
-		}
+		//For each block of size "scale" calculate the local linear trend,
+		//subtract it from the time series, and calculate the RMS of this residue
+		
 		return x;
 	}
-	
-	
-	
+
+
+
 	public static void main(String[] args){
 		ArrayList<Future<Double>> futures=
 				new ArrayList<Future<Double>>(THREADS);
@@ -89,7 +73,6 @@ public class Dfa implements Callable<Double>{
 				Executors.newFixedThreadPool(THREADS);
 
 		int N=80;
-		double sum=0;
 		int[] scales={5,10,15,20,25,30,35,40,45,50,55,60,65,70};
 		double[] data=new double[N];
 		for(int i=0;i<N;i++)
@@ -105,18 +88,20 @@ public class Dfa implements Callable<Double>{
 				Future<Double> future= executor.submit(dfa);
 				futures.add(future);
 			}
-		
+
 			for( Future<Double> future: futures){
-				sum+=future.get();
+				future.isDone();
 			}
+
 			long end=System.currentTimeMillis();
-			System.out.println("sum is = " + sum + " time= "+
-					(end-start)/1000.0);
+			System.out.println("Results: ");
+			for(double r : dfa.results)
+				System.out.print(r + " ");
+			System.out.println("Time= " + (end-start)/1000.0);
+
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		}
+		} 
 		executor.shutdown();
 
 	}
