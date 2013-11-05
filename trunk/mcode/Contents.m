bc4d6690c209868aa0b16894e4c179953e9aee92
ikarosilva00@gmail.com <ikarosilva00@gmail.com@e73a84c7-bd09-c1a9-13a7-bc916e2add03>
2013-11-05 15:12:54 +0000

    added better diagnostic tools

--- trunk/mcode/Contents.m
+++ trunk/mcode/Contents.m
@@ -1,7 +1,7 @@
 %  WaveForm DataBase (WFDB) Toolbox 
-%  Version 0.9.4.1 
+%  Version 0.9.4.2 
 % 
-%  Last Updated November 4, 2013
+%  Last Updated November 5, 2013
 %
 %
 %This is a set of MATLAB functions and wrappers for reading, writing, and processing
