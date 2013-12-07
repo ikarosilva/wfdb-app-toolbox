1b9b8662bfaed93e3f8bff306c50ae348f91e02d
ikarosilva00@gmail.com <ikarosilva00@gmail.com@e73a84c7-bd09-c1a9-13a7-bc916e2add03>
2013-12-07 17:04:31 +0000

    Added capability for batch downloads of databases in PHYSIONETDB and fixed problem with outputting record list in RDMIMIC2WAVE.

--- mcode/physionetdb.m
+++ mcode/physionetdb.m
@@ -1,6 +1,6 @@
 function varargout=physionetdb(varargin)
 %
-% db_list=physionetdb(db_name)
+% db_list=physionetdb(db_name,DoBatchDownload)
 %
 %
 % Lists all the available databases at PhysioNet
@@ -16,6 +16,12 @@ function varargout=physionetdb(varargin)
 %          number of records so that querying the records in the database
 %          can take a long time.
 %
+% DoBatchDownload
+%          If 'db_name' is present, setting this flag to true
+%          (DoBatchDownload=1), will download all records of the database
+%          db_name to a subdirectory in the current directory called
+%          'db_name'. Default is false. Note: requires that the user have
+%          write permission to the current directory.
 %
 % Output Parameters
 % db_list -(Optional) Cell array list of elements. If an output
@@ -27,8 +33,8 @@ function varargout=physionetdb(varargin)
 %
 % Author: Ikaro Silva, 2013
 % Since: 0.0.1
+% Last Modified: December 12, 2013
 %
-% Example:
 %
 % %Example 1 - List all available databases from PhysioNet into the screen
 % physionetdb
@@ -36,7 +42,8 @@ function varargout=physionetdb(varargin)
 % %Example 2- List all available signals in the ucddb database.
 % physionetdb('ucddb')
 %
-
+% %Example 3- Download all records for database MITDB
+%  rc=physionetdb('mitdb',1);
 persistent isloaded
 
 if(isempty(isloaded) || ~isloaded)
@@ -44,8 +51,9 @@ if(isempty(isloaded) || ~isloaded)
     isloaded=wfdbloadlib;
 end
 
-inputs={'db_name'};
+inputs={'db_name','DoBatchDownload'};
 db_name=[];
+DoBatchDownload=0;
 for n=1:nargin
     if(~isempty(varargin{n}))
         eval([inputs{n} '=varargin{n};'])
@@ -68,13 +76,26 @@ if(isempty(db_name))
     end
 else
     J=org.physionet.wfdb.physiobank.PhysioNetDB(db_name);
+    if(DoBatchDownload)
+        display(['Making directory: ' db_name ' to store record files'])
+        mkdir(db_name)
+        wfdb_url='http://physionet.org/physiobank/database/';
+    end
     if(nargout>0)
         rec_list=J.getDBRecordList;
         db_list={};
+        Nstr=num2str(double(rec_list.size));
         for i=0:double(rec_list.size)-1
             sig_list=rec_list.get(i).getSignalList;
             for j=0:double(sig_list.size)-1
                 db_list(end+1)=cell(sig_list.get(j).getRecordName);
+            end
+            if(DoBatchDownload)
+                recName=cell(rec_list.get(i).getRecordName);
+                recName=recName{:};
+                display(['Downloading record (' num2str(i+1) ' / ' Nstr ') : ' recName])
+                [filestr1] = urlwrite([wfdb_url recName '.dat'],[recName '.dat']);
+                [filestr2] = urlwrite([wfdb_url recName '.hea'],[recName '.hea']);
             end
         end
         varargout(1)={db_list};
