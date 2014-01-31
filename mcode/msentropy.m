518b7ee31a6a07eb9a43c19536e22053eb4facb5
ikarosilva00@gmail.com <ikarosilva00@gmail.com@e73a84c7-bd09-c1a9-13a7-bc916e2add03>
2014-01-31 16:19:53 +0000

    MSENTROPY now compatible with Octave. Added extra example to WFDBDEMO

--- mcode/msentropy.m
+++ mcode/msentropy.m
@@ -10,12 +10,12 @@ function varargout=msentropy(varargin)
 % http://www.physionet.org/physiotools/mse/tutorial/
 %
 %
-% Please cite these publications when referencing this material: 
+% Please cite these publications when referencing this material:
 %     Costa M., Goldberger A.L., Peng C.-K. Multiscale entropy analysis of biological signals. Phys Rev E 2005;71:021906.
 %     Costa M., Goldberger A.L., Peng C.-K. Multiscale entropy analysis of physiologic time series. Phys Rev Lett 2002; 89:062102.
-% 
+%
 % Also include the standard citation for PhysioNet:
-%     Goldberger AL, Amaral LAN, Glass L, Hausdorff JM, Ivanov PCh, Mark RG, 
+%     Goldberger AL, Amaral LAN, Glass L, Hausdorff JM, Ivanov PCh, Mark RG,
 %     Mietus JE, Moody GB, Peng C-K, Stanley HE. PhysioBank, PhysioToolkit, and PhysioNet: components of a new research resource for complex physiologic signals. Circulation 101(23):e215-e220 [Circulation Electronic Pages; http://circ.ahajournals.org/cgi/content/full/101/23/e215]; 2000 (June 13)
 %
 % Readers of may also wish to read:
@@ -34,7 +34,7 @@ function varargout=msentropy(varargin)
 % dr
 %       1x1 double. Sets the scale increment to dr (>0; default: 0.05).
 % N
-%       1x1 integer. Stop the analysis with row N. 
+%       1x1 integer. Stop the analysis with row N.
 %       By default, analysis ends at row 39999, or at the end of the data set if there are fewer rows.
 % N0
 %       1x1 integer. Begin the analysis with row N0.
@@ -50,25 +50,25 @@ function varargout=msentropy(varargin)
 % maxR
 %        1x1 double > 0. Set the maximum m to maxR (>0; default: 0.15).
 %
-% 
+%
 % Outputs:
 % y
 %       A 1xM vector of doubles corresponding to estimated sample entropies at each scale.
 % scale
 %       A 1xM vector of integers specifying the scales in which 'y' was
 %       estimated.
-% 
-% info 
+%
+% info
 %       An optional 3x1 cell array of strings providing loggin and verbose information from
-%       the calculation. 
+%       the calculation.
 %
 % Wrapper written by Ikaro Silva, 2013
-% Last Modified: November , 2013
+% Last Modified: January 30, 2014
 % Version 0.0.1
 %
 % Since 0.9.5
 %
-% %Example 
+% %Example
 % N=30000;
 % noise=randn(N,1);
 % maxScale=10;
@@ -86,9 +86,9 @@ function varargout=msentropy(varargin)
 % See also WFDBDESC, PHYSIONETDB, RDANN, ANN2RR, MAPRECORD
 
 %endOfHelp
-persistent javaWfdbExec
+persistent javaWfdbExec config
 if(isempty(javaWfdbExec))
-    javaWfdbExec=getWfdbClass('mse');
+    [javaWfdbExec,config]=getWfdbClass('mse');
 end
 
 %Set default pararamter values
@@ -108,6 +108,7 @@ wfdb_argument={};
 info=[];
 scale=[];
 y=[];
+x=[];
 for n=1:nargin
     if(~isempty(varargin{n}))
         eval([inputs{n} '=varargin{n};'])
@@ -154,7 +155,18 @@ if(~isempty(maxR))
     wfdb_argument{end+1}=[num2str(maxR)];
 end
 javaWfdbExec.setArguments(wfdb_argument);
-out=cell(javaWfdbExec.execWithStandardInput(x).toArray);
+
+if(config.inOctave)
+    x=cellstr(num2str(x));
+    x=java2mat(javaWfdbExec.execWithStandardInput(x));
+    Nx=x.size;
+    out=cell(Nx,1);
+    for n=1:Nx
+        out{n}=x.get(n-1);
+    end
+else
+    out=cell(javaWfdbExec.execWithStandardInput(x).toArray);
+end
 M=length(out);
 if(M<4)
     error(['Error calculating MSE:' out{:}])
