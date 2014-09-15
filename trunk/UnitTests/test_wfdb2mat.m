52d4cf8e459c0e723fd29bc55fd2ddae8ec7242d
ikarosilva00@gmail.com <ikarosilva00@gmail.com@e73a84c7-bd09-c1a9-13a7-bc916e2add03>
2014-09-15 15:26:10 +0000

    Added unit testing for WFDB2MAT

---
+++ trunk/UnitTests/test_wfdb2mat.m
@@ -0,0 +1,23 @@
+function [tests,pass,perf]=test_wfdb2mat(varargin)
+
+
+ 
+  %Generate 3 different signals and convert them to signed 16 bit in WFDB format
+str1=['wfdb2mat(''mitdb/200'');[tm,signal,Fs,siginfo]=rdmat(''200m'');'...
+    '[tm2,signal2]=rdsamp(''200m'');sum(abs(signal-signal2));'];
+  
+cln1=['delete([pwd filesep ''200m*'']);'];
+          
+inputs={'verbose'};
+verbose=0;
+for n=1:nargin
+    if(~isempty(varargin{n}))
+        eval([inputs{n} '=varargin{n};'])
+    end
+end
+
+%Test the examples 
+test_string={str1};
+clean_up={cln1};
+      
+[tests,pass,perf]=test_wrapper(test_string,clean_up,verbose);
