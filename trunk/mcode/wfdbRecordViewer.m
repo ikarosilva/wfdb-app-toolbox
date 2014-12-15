5d90cdee366e4ff853c91b803845f08cf6eae6ac
ikarosilva00@gmail.com <ikarosilva00@gmail.com@e73a84c7-bd09-c1a9-13a7-bc916e2add03>
2014-12-15 19:14:50 +0000

    fixing several bugs and adding new feature to wfbRecordViewer

--- trunk/mcode/wfdbRecordViewer.m
+++ trunk/mcode/wfdbRecordViewer.m
@@ -22,7 +22,7 @@ function varargout = wfdbRecordViewer(varargin)
 
 % Edit the above text to modify the response to help wfdbRecordViewer
 
-% Last Modified by GUIDE v2.5 12-Dec-2014 16:40:29
+% Last Modified by GUIDE v2.5 15-Dec-2014 12:31:50
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -58,17 +58,22 @@ handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
-directoryname = uigetdir('', 'Select Database Directory');
+[filename,directoryname] = uigetfile('*.hea','Select signal header file:');
 cd(directoryname)
 tmp=dir('*.hea');
 
 N=length(tmp);
 records=cell(N,1);
+current_record=[];
 for n=1:N
-    records(n)={tmp(n).name(1:end-4)};
+    fname=tmp(n).name;
+    records(n)={fname(1:end-4)};
+    if(strcmp(fname,filename))
+        current_record=n;
+    end
 end
-current_record=1;
-set(handles.popupmenu1,'String',records)
+set(handles.RecorListMenu,'String',records)
+set(handles.RecorListMenu,'Value',current_record)
 loadRecord(records{current_record})
 loadAnnotationList(records{current_record},handles);
 set(handles.slider1,'Max',tm(end))
@@ -76,62 +81,33 @@ set(handles.slider1,'Min',tm(1))
 set(handles.slider1,'SliderStep',[1 1]);
 sliderStep=get(handles.slider1,'SliderStep');
 tm_step=(tm(end)-tm(1)).*sliderStep(1);
-if strcmp(get(hObject,'Visible'),'off')
-    wfdbplot(handles)
-end
+
+wfdbplot(handles)
+analysisplot(handles)
 
 
 
-
-% UIWAIT makes wfdbRecordViewer wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
-
-% --- Outputs from this function are returned to the command line.
 function varargout = wfdbRecordViewer_OutputFcn(~,~, handles)
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
 % Get default command line output from handles structure
 varargout{1} = handles.output;
 
-% --- Executes on button press in pushbutton1.
-function pushbutton1_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-axes(handles.axes1);
-cla;
+function PreviousButton_Callback(hObject, eventdata, handles)
 global current_record records
 
 current_record=current_record - 1;
-set(handles.popupmenu1,'Value',current_record);
-loadRecord(records{current_record})
-loadAnnotationList(records{current_record},handles)
-wfdbplot(handles)
-Ann1_CreateFcn(hObject, eventdata, handles)
-Ann2_CreateFcn(hObject, eventdata, handles)
-CompareAnn_Callback(hObject, eventdata, handles)
+set(handles.RecorListMenu,'Value',current_record);
+Refresh(hObject, eventdata, handles)
 
-% --- Executes on button press in pushbutton4.
-function pushbutton4_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton4 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-axes(handles.axes1);
-cla;
-global current_record records 
 
+function NextButton_Callback(hObject, eventdata, handles)
+global current_record records
 current_record=current_record + 1;
-set(handles.popupmenu1,'Value',current_record);
-loadRecord(records{current_record})
-loadAnnotationList(records{current_record},handles)
-wfdbplot(handles)
-Ann1_CreateFcn(hObject, eventdata, handles)
-Ann2_CreateFcn(hObject, eventdata, handles)
-CompareAnn_Callback(hObject, eventdata, handles)
+set(handles.RecorListMenu,'Value',current_record);
+Refresh(hObject, eventdata, handles)
 
 
 % --------------------------------------------------------------------
@@ -143,9 +119,6 @@ function FileMenu_Callback(hObject, eventdata, handles)
 
 % --------------------------------------------------------------------
 function OpenMenuItem_Callback(hObject, eventdata, handles)
-% hObject    handle to OpenMenuItem (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 file = uigetfile('*.fig');
 if ~isequal(file, 0)
     open(file);
@@ -153,16 +126,10 @@ end
 
 % --------------------------------------------------------------------
 function PrintMenuItem_Callback(hObject, eventdata, handles)
-% hObject    handle to PrintMenuItem (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 printdlg(handles.figure1)
 
 % --------------------------------------------------------------------
 function CloseMenuItem_Callback(hObject, eventdata, handles)
-% hObject    handle to CloseMenuItem (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 selection = questdlg(['Close ' get(handles.figure1,'Name') '?'],...
     ['Close ' get(handles.figure1,'Name') '...'],...
     'Yes','No','Yes');
@@ -173,24 +140,16 @@ end
 delete(handles.figure1)
 
 
-% --- Executes on selection change in popupmenu1.
-function popupmenu1_Callback(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+% --- Executes on selection change in RecorListMenu.
+function RecorListMenu_Callback(hObject, eventdata, handles)
 
-% Hints: contents = get(hObject,'String') returns popupmenu1 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+global current_record records 
+current_record=get(handles.RecorListMenu,'Value');
+Refresh(hObject, eventdata, handles)
 
 
 % --- Executes during object creation, after setting all properties.
-function popupmenu1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
+function RecorListMenu_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
@@ -198,44 +157,37 @@ end
 
 % --- Executes on slider movement.
 function slider1_Callback(hObject, eventdata, handles)
-% hObject    handle to slider1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'Value') returns position of slider
-%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
-axes(handles.axes1);
-cla;
 wfdbplot(handles)
 
 
 % --- Executes during object creation, after setting all properties.
 function slider1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to slider1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: slider controls usually have a light gray background.
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
 
 function loadRecord(fname)
 global tm signal info
+h = waitbar(0,'Loading Data. Please wait...');
 try
     [tm,signal]=rdmat(fname);
 catch
     [tm,signal]=rdsamp(fname);
 end
 info=wfdbdesc(fname);
+close(h)
 
 function loadAnn1(fname,annName)
 global ann1
+h = waitbar(0,'Loading Annotations. Please wait...');
 [ann1,type,subtype,chan,num,comments]=rdann(fname,annName);
+close(h)
 
 function loadAnn2(fname,annName)
 global ann2
+h = waitbar(0,'Loading Annotations. Please wait...');
 [ann2,type,subtype,chan,num,comments]=rdann(fname,annName);
+close(h)
 
 function loadAnnotationList(fname,handles)
 global ann1 ann2 annDiff
@@ -262,13 +214,14 @@ for i=1:length(tmp)
     end
 end
 
-set(handles.Ann1,'String',annotations)
-set(handles.Ann2,'String',annotations)
+set(handles.Ann1Menu,'String',annotations)
+set(handles.Ann2Menu,'String',annotations)
 
 
 function wfdbplot(handles)
-
 global tm signal info tm_step ann1 ann2 annDiff
+axes(handles.axes1);
+cla;
 
 %Normalize each signal and plot them with an offset
 [N,CH]=size(signal);
@@ -305,33 +258,33 @@ for ch=1:CH;
         tmp_ann2=ann2((ann2>ind_start) & (ann2<ind_end));
         plot(tm(tmp_ann2),OFFSET(ch),'r*','MarkerSize',5,'MarkerFaceColor','r')
     end
-    text(tm(ind_start),ch*offset+offset/2,info(ch).Description,'FontWeight','bold','FontSize',12)
+    text(tm(ind_start),ch*offset+0.8*offset,info(ch).Description,'FontWeight','bold','FontSize',12)
     
 end
-if(~isempty(annDiff))
-    df=annDiff-min(annDiff((ann1>ind_start) & (ann1<ind_end)));
-    df=offset.*df./max(df((ann1>ind_start) & (ann1<ind_end)));
-    df=df((ann1>ind_start) & (ann1<ind_end));
-    plot(tm(tmp_ann1),df,'k*-')
-    text(tm(tmp_ann1(1)),offset/2,'Ann Diff','FontWeight','bold','FontSize',12)
-    
-end
-
 set(gca,'YTick',[])
 set(gca,'YTickLabel',[])
 set(gca,'FontSize',10)
 set(gca,'FontWeight','bold')
-xlabel('Time (s)')
+xlabel('Time (seconds)')
+
+%Plot annotations in analysis window
+if(~isempty(annDiff) & (get(handles.AnalysisMenu,'Value')==1))
+    axes(handles.AnalysisAxes);
+    df=annDiff((ann1>ind_start) & (ann1<ind_end));
+    plot(tm(tmp_ann1),df,'k*-')
+    text(tm(tmp_ann1(1)),max(df),'Ann Diff','FontWeight','bold','FontSize',12)
+    grid on
+    ylabel('Diff (seconds)')
+end
+
+
+function analysisplot(handles)
+
+
 
 
 % --- Executes on selection change in TimeScaleSelection.
 function TimeScaleSelection_Callback(hObject, eventdata, handles)
-% hObject    handle to TimeScaleSelection (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns TimeScaleSelection contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from TimeScaleSelection
 global tm_step tm
 
 TM_SC=[tm(end)-tm(1) 60*2 30*2 10*2 5*2 1*2];
@@ -347,12 +300,6 @@ wfdbplot(handles)
 
 % --- Executes during object creation, after setting all properties.
 function TimeScaleSelection_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to TimeScaleSelection (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
@@ -360,112 +307,82 @@ end
 
 % --- Executes on selection change in AmplitudeScale.
 function AmplitudeScale_Callback(hObject, eventdata, handles)
-% hObject    handle to AmplitudeScale (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns AmplitudeScale contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from AmplitudeScale
-axes(handles.axes1);
-cla;
 wfdbplot(handles)
 
 
 % --- Executes during object creation, after setting all properties.
 function AmplitudeScale_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to AmplitudeScale (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
 
-% --- Executes on selection change in Ann1.
-function Ann1_Callback(hObject, eventdata, handles)
-% hObject    handle to Ann1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns Ann1 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from Ann1
+% --- Executes on selection change in Ann1Menu.
+function Ann1Menu_Callback(hObject, eventdata, handles)
 global ann1 records current_record
 
-ind = get(handles.Ann1, 'Value');
-annStr=get(handles.Ann1, 'String');
+ind = get(handles.Ann1Menu, 'Value');
+annStr=get(handles.Ann1Menu, 'String');
 if(ind ~=1)
     loadAnn1(records{current_record},annStr{ind})
-    axes(handles.axes1);
-    cla;
     wfdbplot(handles)
 end
 
 
 % --- Executes during object creation, after setting all properties.
-function Ann1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to Ann1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
+function Ann1Menu_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
 
-% --- Executes on selection change in Ann2.
-function Ann2_Callback(hObject, eventdata, handles)
-% hObject    handle to Ann2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = cellstr(get(hObject,'String')) returns Ann2 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from Ann2
+% --- Executes on selection change in Ann2Menu.
+function Ann2Menu_Callback(hObject, eventdata, handles)
 global ann2 records current_record
 
-ind = get(handles.Ann2, 'Value');
-annStr=get(handles.Ann2, 'String');
+ind = get(handles.Ann2Menu, 'Value');
+annStr=get(handles.Ann2Menu, 'String');
 if(ind ~=1)
     loadAnn2(records{current_record},annStr{ind})
-    axes(handles.axes1);
-    cla;
     wfdbplot(handles)
 end
 
 % --- Executes during object creation, after setting all properties.
-function Ann2_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to Ann2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
+function Ann2Menu_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
 
-% --- Executes on button press in CompareAnn.
-function CompareAnn_Callback(hObject, eventdata, handles)
-% hObject    handle to CompareAnn (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+function AnalysisMenu_Callback(hObject, eventdata, handles)
 
-% Hint: get(hObject,'Value') returns toggle state of CompareAnn
-global tm  signals ann1 ann2 annDiff info
-
-value=get(hObject,'Value');
-annDiff=[];
-if(value==1)
-    %Compare annotation with ann1 being the reference
+if(get(handles.AnalysisMenu,'Value')==1)
+    global tm  signals ann1 ann2 annDiff info
+    h = waitbar(0,'Comparing Annotations. Please wait...');
+    annDiff=[];
+    %Compare annotation with ann1menu being the reference
     N=length(ann1);
+    Fs=double(info(1).SamplingFrequency);
     if(~isempty(ann2))
         [A1,A2]=meshgrid(ann1,ann2);
-        annDiff=min(abs(A1-A2)).*double(info(1).SamplingFrequency);
+        annDiff=min(abs(A1-A2))./Fs;
     end
+    close(h)
+    wfdbplot(handles)
 end
-wfdbplot(handles)
+
+
+function AnalysisMenu_CreateFcn(hObject, eventdata, handles)
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function Refresh(hObject, eventdata, handles)
+global records current_record
+
+loadRecord(records{current_record})
+loadAnnotationList(records{current_record},handles)
+Ann1Menu_Callback(hObject, eventdata, handles)
+Ann2Menu_Callback(hObject, eventdata, handles)
+AnalysisMenu_Callback(hObject, eventdata, handles)
