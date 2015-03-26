603cb33bc2384a23a06c3d34ab1b8f8317dc6893
ikarosilva <ikaro@mit.edu>
2015-03-26 10:40:05 -0400

    init of JNI implementation of Rdsamp

--- src/org/physionet/wfdb/jni/Rdsamp.java
+++ src/org/physionet/wfdb/jni/Rdsamp.java
@@ -3,12 +3,15 @@ package org.physionet.wfdb.jni;
 class Rdsamp {
 	public native void getData();
 
+	
 	static {
 		System.loadLibrary("rdsampjni");
 	}
-
+	
 	public static void main(String[] args) {
 		Rdsamp myRdsamp = new Rdsamp();
+		System.out.println("testing Rdsamp...");
 		myRdsamp.getData();
+		System.out.println("Done testing Rdsamp...");
 	}
 }
