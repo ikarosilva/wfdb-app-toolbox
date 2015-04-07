4a08f7097aa27cf89acaf652c893ddd2ae79384c
ikarosilva <ikaro@mit.edu>
2015-04-07 15:59:14 -0400

    Removed support to Java 6

--- mcode/rdsamp.m
+++ mcode/rdsamp.m
@@ -83,7 +83,7 @@ function varargout=rdsamp(varargin)
 
 %endOfHelp
 
-persistent javaWfdbExec javaWfdbRdsamp config
+persistent javaWfdbExec config
 if(isempty(javaWfdbExec))
     [javaWfdbExec,config]=getWfdbClass('rdsamp');
 end
@@ -109,7 +109,7 @@ end
 
 %Cache record
 wfdbdownload(recordName);
-
+javaWfdbRdsamp=[];
 if(isempty(javaWfdbRdsamp) && (rawUnits ==0))
     javaWfdbRdsamp=javaObject('org.physionet.wfdb.jni.Rdsamp');
 end
