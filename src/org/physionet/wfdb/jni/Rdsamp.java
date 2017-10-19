d7f8e3d8eb5a2dac975fafcb6dbc8c0d7e3ef95f
Benjamin Moody <benjaminmoody@gmail.com>
2017-10-19 12:17:01 -0400

    Rdsamp: Do not try to load liblibwfdb.
    This line must have been added at some point in an attempt to fix a
    broken version of librdsampjni that didn't import its dependencies
    correctly.
    
    On all supported platforms, shared libraries are responsible for
    managing their own dependencies.

--- src/org/physionet/wfdb/jni/Rdsamp.java
+++ src/org/physionet/wfdb/jni/Rdsamp.java
@@ -22,7 +22,6 @@ public class Rdsamp {
 	
 	//Initialize enviroment
 	static {
-		org.physionet.wfdb.SystemSettings.loadLib("libwfdb");
 		org.physionet.wfdb.SystemSettings.loadLib("rdsampjni");
 	}
 	
