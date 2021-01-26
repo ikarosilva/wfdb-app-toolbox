f6ff845c953bb08e7832889be1982f5f23e97065
Lucas McCullum <lumccul3@gmail.com>
2021-01-26 07:40:06 -0500

    Fixes whitespace in Java files

--- src/org/physionet/wfdb/physiobank/PhysioNetDB.java
+++ src/org/physionet/wfdb/physiobank/PhysioNetDB.java
@@ -52,245 +52,243 @@ import java.util.regex.Pattern;
 
 
 public class PhysioNetDB {
-    private String name;
-    private String info;
-    private URL url;
-    private static final String DB_URL="https://physionet.org/content/";
-    private static final String DB_LIST="https://physionet.org/rest/database-list/";
-    private ArrayList<PhysioNetRecord> dbRecordList;
-    private static Logger logger =
-        Logger.getLogger(PhysioNetRecord.class.getName());
+	private String name;
+	private String info;
+	private URL url;
+	private static final String DB_URL="https://physionet.org/content/";
+	private static final String DB_LIST="https://physionet.org/rest/database-list/";
+	private ArrayList<PhysioNetRecord> dbRecordList;
+	private static Logger logger =
+			Logger.getLogger(PhysioNetRecord.class.getName());
 
-    public PhysioNetDB(String Name){
-        name=Name;
-        url=setDBURL();
-        info=setInfo();
-        dbRecordList = new ArrayList<PhysioNetRecord>();
-    }
+	public PhysioNetDB(String Name){
+		name=Name;
+		url=setDBURL();
+		info=setInfo();
+		dbRecordList = new ArrayList<PhysioNetRecord>();
+	}
 
-    private PhysioNetDB(String Name,String Info){
-        name=Name;
-        info=Info;
-        url=setDBURL();
-        dbRecordList = new ArrayList<PhysioNetRecord>();
-    }
+	private PhysioNetDB(String Name,String Info){
+		name=Name;
+		info=Info;
+		url=setDBURL();
+		dbRecordList = new ArrayList<PhysioNetRecord>();
+	}
 
-    public String getname() {
-        return name;
-    }
+	public String getname() {
+		return name;
+	}
+	public ArrayList<PhysioNetRecord> getDBRecordList() throws Exception{
+		if(dbRecordList.isEmpty()){
+			this.setDBRecordList();
+		}
+		return dbRecordList;
+	}
+	public String getinfo() {
+		return info;
+	}
+	public URL getURL() {
+		return url;
+	}
 
-    public ArrayList<PhysioNetRecord> getDBRecordList() throws Exception{
-        if(dbRecordList.isEmpty()){
-            this.setDBRecordList();
-        }
-        return dbRecordList;
-    }
+	public static List<String> getDBInfo(String regex,String inputText){
+		List<String> dbInfoArray = new ArrayList<String>();
+		Matcher dbInfo = Pattern.compile(regex)
+			.matcher(inputText);
+		while (dbInfo.find()) {
+			dbInfoArray.add(dbInfo.group(1));
+		}
+		return dbInfoArray;
+	}
 
-    public String getinfo() {
-        return info;
-    }
+	public static List<PhysioNetDB> getPhysioNetDBList(){
+		String inputText;
+		BufferedReader in = null;
+		List<PhysioNetDB> physionetDBList = new ArrayList<PhysioNetDB>();
+		try {
+			URL oracle = new URL(DB_LIST);
+			in = new BufferedReader(
+					new InputStreamReader(oracle.openStream()));
+			String tmpURL;
+			String tmpName;
+			String tmpInfo;
+			// Get the database list using the REST API and parsing the
+			// resulting JSON format.. It should be a single line
+			inputText = in.readLine();
+			List<String> projectTitles = getDBInfo("\"title\": \"([^\"]*)",inputText);
+			List<String> projectVersions = getDBInfo("\"version\": \"([^\"]*)",inputText);
+			List<String> projectSlugs = getDBInfo("\"slug\": \"([^\"]*)",inputText);
+			List<String> projectURLs = new ArrayList<String>();
+			// All the arrays should be the same size
+			for(int i=0; i<projectTitles.size(); i++){
+				// Get the URL
+				tmpURL = DB_URL+projectSlugs.get(i)+"/"+projectVersions.get(i)+"/";
+				projectURLs.add(tmpURL);
+				logger.finest("\n\t***Reading URL: \n\t" + tmpURL);
+				tmpName=projectSlugs.get(i);
+				tmpInfo=projectTitles.get(i);
+				physionetDBList.add(new PhysioNetDB(tmpName,tmpInfo));
+			}
+			logger.fine("\n\t*** physionetDBList Size: \n\t" + 
+					physionetDBList.size());
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return physionetDBList;
+	}
 
-    public URL getURL() {
-        return url;
-    }
+	public static HashMap<String,PhysioNetDB> getPhysioNetDBMap(){
+		String inputLine;
+		BufferedReader in = null;
+		HashMap<String,PhysioNetDB> physionetDBMap= new HashMap<String,PhysioNetDB>();
+		try {
+			URL oracle = new URL(DB_LIST);
+			in = new BufferedReader(
+					new InputStreamReader(oracle.openStream()));
+			String[] tmpStr;
+			String tmpname;
+			String tmpInfo;
+			while ((inputLine = in.readLine()) != null){
+				logger.finest("\n\t***Reading URL: \n\t" + inputLine);
+				tmpStr=inputLine.split("\\t");
+				tmpname=tmpStr[0];
+				tmpInfo=(inputLine.replaceFirst(tmpname,"")).replaceAll("\\t","");
+				physionetDBMap.put(tmpname,new PhysioNetDB(tmpname,tmpInfo));
+			}			
+			in.close();
+			logger.fine("\n\t*** physionetDBMap Size: \n\t" + 
+					 physionetDBMap.size());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return physionetDBMap;
+	}
 
-    public static List<String> getDBInfo(String regex,String inputText){
-        List<String> dbInfoArray = new ArrayList<String>();
-        Matcher dbInfo = Pattern.compile(regex)
-            .matcher(inputText);
-        while (dbInfo.find()) {
-            dbInfoArray.add(dbInfo.group(1));
-        }
-        return dbInfoArray;
-    }
+	public void printDBInfo(){
+		System.out.println(name);
+		System.out.println("\tDescription: "+ info);
+		System.out.println("\tURL: "+ url);
+	}
 
-    public static List<PhysioNetDB> getPhysioNetDBList(){
-        String inputText;
-        BufferedReader in = null;
-        List<PhysioNetDB> physionetDBList = new ArrayList<PhysioNetDB>();
-        try {
-            URL oracle = new URL(DB_LIST);
-            in = new BufferedReader(
-                new InputStreamReader(oracle.openStream()));
-            String tmpURL;
-            String tmpName;
-            String tmpInfo;
-            // Get the database list using the REST API and parsing the
-            // resulting JSON format.. It should be a single line
-            inputText = in.readLine();
-            List<String> projectTitles = getDBInfo("\"title\": \"([^\"]*)",inputText);
-            List<String> projectVersions = getDBInfo("\"version\": \"([^\"]*)",inputText);
-            List<String> projectSlugs = getDBInfo("\"slug\": \"([^\"]*)",inputText);
-            List<String> projectURLs = new ArrayList<String>();
-            // All the arrays should be the same size
-            for(int i=0; i<projectTitles.size(); i++){
-                // Get the URL
-                tmpURL = DB_URL+projectSlugs.get(i)+"/"+projectVersions.get(i)+"/";
-                projectURLs.add(tmpURL);
-                logger.finest("\n\t***Reading URL: \n\t" + tmpURL);
-                tmpName=projectSlugs.get(i);
-                tmpInfo=projectTitles.get(i);
-                physionetDBList.add(new PhysioNetDB(tmpName,tmpInfo));
-            }
-            logger.fine("\n\t*** physionetDBList Size: \n\t" + 
-                        physionetDBList.size());
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return physionetDBList;
-    }
+	public String getDBInfo(){
+		String str=name + "\n\tDescription: "+ info + "\n\tURL: "+ url;
+		return str;
+	}
 
-    public static HashMap<String,PhysioNetDB> getPhysioNetDBMap(){
-        String inputLine;
-        BufferedReader in = null;
-        HashMap<String,PhysioNetDB> physionetDBMap= new HashMap<String,PhysioNetDB>();
-        try {
-            URL oracle = new URL(DB_LIST);
-            in = new BufferedReader(
-                    new InputStreamReader(oracle.openStream()));
-            String[] tmpStr;
-            String tmpname;
-            String tmpInfo;
-            while ((inputLine = in.readLine()) != null){
-                logger.finest("\n\t***Reading URL: \n\t" + inputLine);
-                tmpStr=inputLine.split("\\t");
-                tmpname=tmpStr[0];
-                tmpInfo=(inputLine.replaceFirst(tmpname,"")).replaceAll("\\t","");
-                physionetDBMap.put(tmpname,new PhysioNetDB(tmpname,tmpInfo));
-            }			
-            in.close();
-            logger.fine("\n\t*** physionetDBMap Size: \n\t" + 
-                     physionetDBMap.size());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return physionetDBMap;
-    }
+	public void printDBRecordList() throws Exception{
 
-    public void printDBInfo(){
-        System.out.println(name);
-        System.out.println("\tDescription: "+ info);
-        System.out.println("\tURL: "+ url);
-    }
-    
-    public String getDBInfo(){
-        String str=name + "\n\tDescription: "+ info + "\n\tURL: "+ url;
-        return str;
-    }
+		this.getDBRecordList();
+		this.printDBInfo();
+		for(PhysioNetRecord rec : dbRecordList){
+			rec.printRecord();
+		}
+	}
 
-    public void printDBRecordList() throws Exception{
-        this.getDBRecordList();
-        this.printDBInfo();
-        for(PhysioNetRecord rec : dbRecordList){
-            rec.printRecord();
-        }
-    }
+	public static void printDBList(List<PhysioNetDB> pnDB ) {
+		// Prints information regarding all databases in pnDB 
+		List<PhysioNetDB> pnDBList = PhysioNetDB.getPhysioNetDBList();
+		for(PhysioNetDB db : pnDBList){
+			db.printDBInfo();
+		}
+	}
 
-    public static void printDBList(List<PhysioNetDB> pnDB ) {
-        // Prints information regarding all databases in pnDB 
-        List<PhysioNetDB> pnDBList = PhysioNetDB.getPhysioNetDBList();
-        for(PhysioNetDB db : pnDBList){
-            db.printDBInfo();
-        }
-    }
+	public static void printDBList() {
+		// Prints information regarding all databases
+		// Currently available at PhysioNet
+		List<PhysioNetDB> pnDBList = PhysioNetDB.getPhysioNetDBList();
+		Collections.sort(pnDBList,PhysioNetDB.DBNameComparator);
+		for(PhysioNetDB db : pnDBList){
+			db.printDBInfo();
+		}
+	}
 
-    public static void printDBList() {
-        // Prints information regarding all databases
-        // Currently available at PhysioNet
-        List<PhysioNetDB> pnDBList = PhysioNetDB.getPhysioNetDBList();
-        Collections.sort(pnDBList,PhysioNetDB.DBNameComparator);
-        for(PhysioNetDB db : pnDBList){
-            db.printDBInfo();
-        }
-    }
+	private URL setDBURL() {
+		logger.finer("\n\t***URL Input: \n\t" + name);
+		try {
+			URL url=new URL(DB_URL + name.replaceAll("/","_"));
+			logger.finer("\n\t***Parsed URL: \n\t" + url.toString());
+			return url;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    private URL setDBURL() {
-        logger.finer("\n\t***URL Input: \n\t" + name);
-        try {
-            URL url=new URL(DB_URL + name.replaceAll("/","_"));
-            logger.finer("\n\t***Parsed URL: \n\t" + url.toString());
-            return url;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	private String setInfo() {
+		String inputLine;
+		BufferedReader in = null;
+		String desc="";
+		try {
+			URL pnb = new URL(DB_LIST);
+			in = new BufferedReader(
+					new InputStreamReader(pnb.openStream()));
+			String[] tmpStr;
+			while ((inputLine = in.readLine()) != null){
+				tmpStr=inputLine.split("\\t");
+				if(tmpStr[0].compareTo(name)==0){
+					desc=(inputLine.replaceFirst(tmpStr[0],"")).replaceAll("\\t","");
+					break;
+				}
+			}			
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		return desc;
+	}
 
-    private String setInfo() {
-        String inputLine;
-        BufferedReader in = null;
-        String desc="";
-        try {
-            URL pnb = new URL(DB_LIST);
-            in = new BufferedReader(
-                new InputStreamReader(pnb.openStream()));
-            String[] tmpStr;
-            while ((inputLine = in.readLine()) != null){
-                tmpStr=inputLine.split("\\t");
-                if(tmpStr[0].compareTo(name)==0){
-                    desc=(inputLine.replaceFirst(tmpStr[0],"")).replaceAll("\\t","");
-                    break;
-                }
-            }			
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }		
-        return desc;
-    }
+	public void setDBRecordList() throws Exception{
+		String inputLine;
+		BufferedReader in = null;
+		String[] tmpStr;
+		String recname="";
+		ArrayList<String> recList = null;
+		logger.finest("\n\t***Getting record list from: \n\t" + url);
+		try {
+			in = new BufferedReader(
+					new InputStreamReader(url.openStream()));
+			while ((inputLine = in.readLine()) != null){
+				tmpStr=inputLine.split("\\t");
+				logger.finest("\n\t***Reading record data: \n\t" + inputLine);
+				if(tmpStr[0].compareTo(recname) != 0){
+					//New record, save the last one and
+					if(!recname.isEmpty()){
+						dbRecordList.add(new PhysioNetRecord(tmpStr[0]));
+					}
+					recname=tmpStr[0];
+					recList=null;
+					recList=new ArrayList<String>();
+				}
+				// Same, record, append to the signal list
+				recList.add(new String(inputLine.replaceFirst(recname,"")));
+			}			
+			in.close();
+			logger.fine("\n\t***Rec List Size: \n\t" + recList.size());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+	}
 
-    public void setDBRecordList() throws Exception{
-        String inputLine;
-        BufferedReader in = null;
-        String[] tmpStr;
-        String recname="";
-        ArrayList<String> recList = null;
-        logger.finest("\n\t***Getting record list from: \n\t" + url);
-        try {
-            in = new BufferedReader(
-                new InputStreamReader(url.openStream()));
-            while ((inputLine = in.readLine()) != null){
-                tmpStr=inputLine.split("\\t");
-                logger.finest("\n\t***Reading record data: \n\t" + inputLine);
-                if(tmpStr[0].compareTo(recname) != 0){
-                    //New record, save the last one and
-                    if(!recname.isEmpty()){
-                        dbRecordList.add(new PhysioNetRecord(tmpStr[0]));
-                    }
-                    recname=tmpStr[0];
-                    recList=null;
-                    recList=new ArrayList<String>();
-                }
-                // Same, record, append to the signal list
-                recList.add(new String(inputLine.replaceFirst(recname,"")));
-            }			
-            in.close();
-            logger.fine("\n\t***Rec List Size: \n\t" + recList.size());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }	
-    }
 
-    public static Comparator<PhysioNetDB> DBNameComparator = new Comparator<PhysioNetDB>() {
-        public int compare(PhysioNetDB db, PhysioNetDB anotherDB) {
-            String Name1 = db.getname().toUpperCase();
-            String Name2 = anotherDB.getname().toUpperCase();
-            if (!(Name1.equals(Name2)))
-                return Name1.compareTo(Name2);
-            else
-                return Name1.compareTo(Name2);
-        }
-    };
+	public static Comparator<PhysioNetDB> DBNameComparator = new Comparator<PhysioNetDB>() {
+		public int compare(PhysioNetDB db, PhysioNetDB anotherDB) {
+			String Name1 = db.getname().toUpperCase();
+			String Name2 = anotherDB.getname().toUpperCase();
+			if (!(Name1.equals(Name2)))
+				return Name1.compareTo(Name2);
+			else
+				return Name1.compareTo(Name2);
+		}
+	};
 
-    public static List<PhysioNetDB> main() {
-        // Prints information regarding all databases
-        // Currently available at PhysioNet
-        return PhysioNetDB.getPhysioNetDBList();
-    }
+	public static List<PhysioNetDB> main() {
+	
+		// Prints information regarding all databases
+		// Currently available at PhysioNet
+		return PhysioNetDB.getPhysioNetDBList();
+	
+	}
 
-    public static void main(String[] args) {
-        main();
-    };
-
+	
 }
